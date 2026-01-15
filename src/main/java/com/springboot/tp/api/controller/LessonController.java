bd36a7c99717642b6f30cfab9c86c005b90fb1f7
Quentin BOISSON <quentin.boisson@hotmail.fr>
2026-01-15 15:29:22 +0100

    Put controllers in conformity with the rest of the code

--- src/main/java/com/springboot/tp/api/controller/LessonController.java
+++ src/main/java/com/springboot/tp/api/controller/LessonController.java
@@ -1,26 +1,30 @@
 package com.springboot.tp.api.controller;
 
-import com.springboot.tp.api.dto.LessonDTO;
+
 import com.springboot.tp.app.service.LessonService;
 import com.springboot.tp.domain.entity.Lesson;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/lessons")
 public class LessonController {
 
     // Appel au service de gestion des leçons
-    private final LessonService lessonService;
+    private final LessonService service;
 
-    public LessonController(LessonService lessonService) {
-        this.lessonService = lessonService;
+    public LessonController(LessonService service) {
+        this.service = service;
     }
 
     @GetMapping
-    public List<LessonDTO> getAllLessons() {
-        return lessonService.getAllLessons();
+    public List<Lesson> getAllLessons() {
+        return service.findAll();
+    }
+
+    @PostMapping("/themes/{themeId}/lessons")
+    Lesson newLessonWithTheme(@RequestBody String title, @PathVariable Long themeId) {
+        return service.create(title, themeId);
     }
 }
--- src/main/java/com/springboot/tp/api/controller/ThemeController.java
+++ src/main/java/com/springboot/tp/api/controller/ThemeController.java
@@ -1,33 +1,21 @@
 package com.springboot.tp.api.controller;
-import com.springboot.tp.domain.entity.Lesson;
+import com.springboot.tp.app.service.ThemeService;
 import com.springboot.tp.domain.entity.Theme;
-import com.springboot.tp.infra.repository.LessonRepository;
-import com.springboot.tp.infra.repository.ThemeRepository;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ThemeController {
-    private final ThemeRepository themeRepository;
-    private final LessonRepository lessonRepository;
+    private final ThemeService service;
 
-    ThemeController(ThemeRepository repository, LessonRepository lessonRepository) {
-        this.themeRepository = repository;
-        this.lessonRepository = lessonRepository;
+    ThemeController(ThemeService service) {
+        this.service = service;
     }
 
     @PostMapping("/themes")
-    Theme newTheme(@RequestBody Theme newTheme) {
-        return themeRepository.save(newTheme);
+    Theme newTheme(@RequestBody String name) {
+        return service.create(name);
     }
 
-    @PostMapping("/themes/{themeId}/lessons")
-    Lesson newLessonWithTheme(@RequestBody String title, @PathVariable Long themeId) {
-        Theme theme = themeRepository.getReferenceById(themeId);
-        Lesson newLesson = new Lesson(title);
-        newLesson.setTheme(theme);
-        return lessonRepository.save(newLesson);
-    }
 }
