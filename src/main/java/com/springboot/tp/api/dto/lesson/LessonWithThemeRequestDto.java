1fdf9629c6fad6225c8de55ef17c5b3bbed45280
Vincent Lagarde <lagardevincent64@gmail.com>
2026-01-15 16:04:44 +0100

    DTO ok

--- src/main/java/com/springboot/tp/api/dto/lesson/LessonWithThemeRequestDto.java
+++ src/main/java/com/springboot/tp/api/dto/lesson/LessonWithThemeRequestDto.java
@@ -1,5 +1,37 @@
 package com.springboot.tp.api.dto.lesson;
 
-public class LessonWithThemeRequestDto {
-    
+public class LessonWithThemeRequestDTO {
+
+    private Long id;
+    private String title;
+    private ThemeResponseDTO theme;
+
+    public LessonWithThemeRequestDTO() {
+    }
+
+    // getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public ThemeResponseDTO getTheme() {
+        return theme;
+    }
+
+    // setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setTheme(ThemeResponseDTO theme) {
+        this.theme = theme;
+    }
 }
