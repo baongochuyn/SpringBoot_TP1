8438c11beee1e382cf0826efcce11d3b71ab3a6b
MathiasHachani <mathias.hachani@gmail.com>
2026-01-15 17:24:55 +0100

    Ajout dépendance pour Optional

--- src/main/java/com/springboot/tp/infra/repository/LessonRepository.java
+++ src/main/java/com/springboot/tp/infra/repository/LessonRepository.java
@@ -4,6 +4,7 @@ import com.springboot.tp.domain.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+import java.util.Optional;
 
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
     
