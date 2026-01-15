b8d078853a95d4a91717ed1ef90647e9ed54c5d6
Bao Ngoc HUYNH <baongochuynh.hbn@gmail.com>
2026-01-15 14:30:10 +0100

    add 2 entities

--- src/main/java/com/springboot/tp/domain/entity/Lesson.java
+++ src/main/java/com/springboot/tp/domain/entity/Lesson.java
@@ -1,5 +1,29 @@
 package com.springboot.tp.domain.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "lessons")
 public class Lesson {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_id", nullable = false)
+    private Theme theme;
+
+    // Constructors
+    public Lesson() {}
+    public Lesson(String title) { this.title = title; }
+
+    // Getters & Setters
+    public Long getId() { return id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public Theme getTheme() { return theme; }
+    public void setTheme(Theme theme) { this.theme = theme; }
 }
