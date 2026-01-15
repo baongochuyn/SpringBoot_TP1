174f341277153270e1efa2ec4d5fea56318d18e6
Quentin BOISSON <quentin.boisson@hotmail.fr>
2026-01-15 15:33:04 +0100

    Changed repositories back

--- src/main/java/com/springboot/tp/infra/repository/LessonRepository.java
+++ src/main/java/com/springboot/tp/infra/repository/LessonRepository.java
@@ -1,8 +1,5 @@
 package com.springboot.tp.infra.repository;
 
-import com.springboot.tp.domain.entity.Lesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonRepository extends JpaRepository<Lesson, Long> {
+public class LessonRepository {
     
 }
--- src/main/java/com/springboot/tp/infra/repository/ThemeRepository.java
+++ src/main/java/com/springboot/tp/infra/repository/ThemeRepository.java
@@ -1,8 +1,5 @@
 package com.springboot.tp.infra.repository;
 
-import com.springboot.tp.domain.entity.Theme;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThemeRepository extends JpaRepository<Theme, Long> {
+public class ThemeRepository {
     
 }
