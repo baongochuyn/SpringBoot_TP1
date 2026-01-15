4291081c6af63ae434a6c4a201c26f44d55e916c
FEUGANG KOUAKEM Ange Corianne <afeugangkouake@et.intechinfo.fr>
2026-01-15 15:41:05 +0100

    adapt to the rest of code

--- src/main/java/com/springboot/tp/app/service/LessonService.java
+++ src/main/java/com/springboot/tp/app/service/LessonService.java
@@ -21,7 +21,7 @@ public class LessonService {
         return repository.save(lesson);
     }
 
-    public List<Lesson> findAll() {
-        return repository.findAll();
+    public List<Lesson> findAllWithTheme() {
+        return repository.findAllWithTheme();
     }
 }
