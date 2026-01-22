d005fbd502e6ec6123eaaf5db1c3c2e6f01e443b
Vincent Lagarde <lagardevincent64@gmail.com>
2026-01-22 14:32:02 +0100

    Mise en place du login

--- src/main/java/com/springboot/tp/api/controller/AuthController.java
+++ src/main/java/com/springboot/tp/api/controller/AuthController.java
@@ -1,5 +1,23 @@
 package com.springboot.tp.api.controller;
 
+import com.springboot.tp.api.dto.Login.LoginRequestDto;
+import com.springboot.tp.api.dto.Login.LoginResponseDto;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
-    
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDto login(@RequestBody LoginRequestDto loginRequestDto) {
+        String token = authService.login(loginRequestDto.getUsername(), loginRequestDto.getPassword());
+        return new LoginResponseDto(token);
+    }
 }
--- src/main/java/com/springboot/tp/api/dto/Login/LoginRequestDto.java
+++ src/main/java/com/springboot/tp/api/dto/Login/LoginRequestDto.java
@@ -1,5 +1,26 @@
 package com.springboot.tp.api.dto.Login;
 
 public class LoginRequestDto {
-    
+
+    private String username;
+    private String password;
+
+    public LoginRequestDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
