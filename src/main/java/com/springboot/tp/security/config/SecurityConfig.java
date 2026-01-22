54422e3e689aa3fc7ccab09056d7dcf20ec542fb
Quentin BOISSON <quentin.boisson@hotmail.fr>
2026-01-22 14:18:55 +0100

    Import de dépendance et début de l'implémentation de la SecurityConfig

--- src/main/java/com/springboot/tp/security/config/SecurityConfig.java
+++ src/main/java/com/springboot/tp/security/config/SecurityConfig.java
@@ -1,5 +1,34 @@
 package com.springboot.tp.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain security(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // API Stateless = pas de CSRF
+                .authorizeHttpRequests(auth -> auth
+                        // Public
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/lessons/**").permitAll()
+
+                        // Privé
+                        .requestMatchers(HttpMethod.POST, "/api/themes").hasRole("ADMIN")
+
+                        // Le reste
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth -> oauth.jwt()) // Active la vérif JWT
+                .build();
+    }
     
 }
