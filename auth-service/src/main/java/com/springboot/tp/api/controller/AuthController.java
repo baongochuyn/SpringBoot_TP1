1ae65738954b8794445e2671e9d31054df54c4b2
Bao Ngoc HUYNH <baongochuynh.hbn@gmail.com>
2026-01-26 20:59:49 +0100

    add gateway + discovery service, move auth to auth-service

--- auth-service/src/main/java/com/springboot/tp/api/controller/AuthController.java
+++ auth-service/src/main/java/com/springboot/tp/api/controller/AuthController.java
@@ -9,7 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService authService;
